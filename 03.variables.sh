93e2ae52404e097dc37654f0b9a69e5a4f3a43e0
srikanth <bommisettysrikanth88@gmail.com>
2024-12-30 23:54:28 +0530

    conversation added

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Srikanth
+PERSON2=Hari
+
+echo "$PERSON!:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you  doing?"
+echo "$PERSON2:: I am good. How are you?"
