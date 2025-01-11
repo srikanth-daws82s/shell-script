e12268f472564fade2620c9be4e86d15ccbd9f53
srikanth <bommisettysrikanth88@gmail.com>
2025-01-11 18:25:46 +0530

    if elif statements added

---
+++ 13-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+STUDENTS=("suresh" "mahesh" "srikanth" "santhosh")
+
+echo "FIRST STUDENT : ${STUDENT[0]}"
+
+echo "SECOND STUDENT : ${STUDENT[1]}"
+
+echo "THIRD STUDENT : ${STUDENT[2]}"
+
+echo "FOURTH STUDENT : ${STUDENT[3]}"
+
+echo "ALL TUDENTS : ${STUDENT[4]}"
