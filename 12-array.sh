6bb29580ed0e9243b91be7fe44b26b309ba70b1f
srikanth <bommisettysrikanth88@gmail.com>
2025-01-11 18:15:39 +0530

    if elif statements added

---
+++ 12-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+movies=("game changer" "dacu maharaj" "sankranthiki vasthunnama")
+# index start with "0" and size is "3"
+
+echo "first movie : ${movies[0]}"
+echo "second movie : ${movies[1]}"
+echo "third movie : ${movies[2]}"
+
+# print the [@] all movies
+echo "print the all movies : ${movies[@]}
