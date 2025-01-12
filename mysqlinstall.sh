64d88c5120f5b06432b66f18db74b9faa31beb1b
srikanth <bommisettysrikanth88@gmail.com>
2025-01-12 11:39:52 +0530

    if elif statements added

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -10,4 +10,22 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "installing mysql...failure"
+    exit 1
+else
+    echo "installing mysql...success"
+
+fi
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing git... failure"
+    exit 1
+else
+    echo "installing git...success"
+
+fi
