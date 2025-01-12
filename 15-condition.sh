42c39681474c3a221a134d3cf6736181013d7ad1
srikanth <bommisettysrikanth88@gmail.com>
2025-01-12 08:28:08 +0530

    if elif statements added

--- 15-condition.sh
+++ 15-condition.sh
@@ -3,8 +3,12 @@
 number=$1
 
 if[ number -gt 100 ]
+then
 
-echo "given number is greaterthan 100"
+     echo "given number is greaterthan 100"
+
 else
-echo "given number is lessthan 100"
+
+     echo "given number is lessthan  or equal to 100"
+
 fi
