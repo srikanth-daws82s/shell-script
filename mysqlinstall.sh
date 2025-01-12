c1c3c415389080f0ebd3de61ad52d8950780c9c4
srikanth <bommisettysrikanth88@gmail.com>
2025-01-12 12:13:46 +0530

    if elif statements added

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -10,19 +10,16 @@ fi
 
 dnf list installed mysql
 if[ $? -ne 0 ] 
-then  # not installed
-
+then  
     dnf install mysql -y
-
     if [ $? -ne 0 ]
     then
-         echo "installing mysql... failure"
+         echo "installing mysql ... failure"
          exit 1
     else
-        echo "installing mysql... success"
+        echo "installing mysql ... success"
 
     fi
-
 else 
      echo "mysql already ... installed"
 
