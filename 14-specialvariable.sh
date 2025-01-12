7e3dc64f03e86fc9e5ee5fcb2286650a48f169d5
srikanth <bommisettysrikanth88@gmail.com>
2025-01-12 07:49:47 +0530

    if elif statements added

--- 14-specialvariable.sh
+++ 14-specialvariable.sh
@@ -7,4 +7,5 @@ echo "Present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Process id of current script: $$"
+sleep 60 &
 echo "Process id of last command in background: $!"
