82397dea38fe8396676b7c1e3be9354f641c776c
srikanth <bommisettysrikanth88@gmail.com>
2025-01-13 09:41:48 +0530

    if elif statements added

---
+++ 17-colors.sh
@@ -0,0 +1,51 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+     then
+          echo "$2... $RED failure"
+          exit 1
+     else
+        echo "$2... $GREEN success"
+
+    fi  
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] 
+then 
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+
+else 
+     echo "mysql already ... $YELLOW installed"
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "installing git"
+    
+else
+    echo "git already ... $YELLOW installed"
+
+fi
+
+
