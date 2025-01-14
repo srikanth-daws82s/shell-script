1d580c417cdce3bfa4ff82df8e166b9d7f4a7def
srikanth <bommisettysrikanth88@gmail.com>
2025-01-14 10:33:12 +0530

    if elif statements added

---
+++ 18-logfiles.sh
@@ -0,0 +1,57 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs" 
+LOG_FILE=$(echo $0 | cut -d "," -f1)
+TIMESTAMP=$(date +"%d-%m-%y-%H-%S-%M")
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+     then
+          echo "$2... $R failure"
+          exit 1
+     else
+        echo "$2... $G success"
+
+    fi  
+
+}
+
+echo script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ] 
+then 
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "installing mysql"
+
+else 
+     echo  -e "mysql already ... $Y installed"
+fi
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "installing git"
+    
+else
+    echo  -e "git already ... $Y installed"
+
+fi
+
+
